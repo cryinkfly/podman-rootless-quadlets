96ee759bd022074b4f847f04646fde2b78043ec7
Steve Zabka <info@cryinkfly.de>
2025-12-17 20:47:40 +0100

    Create initial-setup.sh

---
+++ quadlets-dev/opencloud-minimal/initial-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# initial-setup.sh
+# Script to initialize OpenCloud rootless Podman container
+
+# Hardcoded admin password
+ADMIN_PASSWORD="YourSecurePassword123"
+
+# Create required volumes if they don't exist
+podman volume create opencloud_config
+podman volume create opencloud_data
+
+# Run OpenCloud initialization
+podman run --rm -it \
+  -v opencloud_config:/etc/opencloud \
+  -v opencloud_data:/var/lib/opencloud \
+  -e IDM_ADMIN_PASSWORD="$ADMIN_PASSWORD" \
+  docker.io/opencloudeu/opencloud-rolling:latest init
